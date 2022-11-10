0de7646fe9e39d22e13e508c7d45a8076a0c415f
Bryan Larson <bryan_larson@hms.harvard.edu>
2022-11-10 11:23:29 -0700

    feat(version): Bumped version to 1.0.0

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bootstrap-dynamic-formsets',
-    version='0.5.0',
+    version='1.0.0',
     packages=['django_bootstrap_dynamic_formsets','django_bootstrap_dynamic_formsets.templatetags'],
     install_requires=['Django','django-bootstrap3',],
     include_package_data=True,
     license='MIT License',
     description='JavaScript-enhanced dynamic formsets using Bootstrap',
-    url='https://github.com/FHBrandenburg/django-bootstrap-dynamic-formsets',
+    url='https://github.com/hms-dbmi/django-bootstrap-dynamic-formsets',
     author='Daniel Weis',
     author_email='weisd@th-brandenburg.de',
     classifiers=[
